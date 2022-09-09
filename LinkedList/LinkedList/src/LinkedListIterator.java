831e0dcfd5b7044f1065497f4fb47e1ee7b066ba
JairChavezB <100861258+JairChavezB@users.noreply.github.com>
2022-09-09 16:24:09 -0500

    LinkedList- Tarea 6

---
+++ LinkedList/LinkedList/src/LinkedListIterator.java
@@ -0,0 +1,13 @@
+package opp.collections.list.linkedList;
+
+public class LinkedListIterator {
+
+    public boolean hasNext() {
+        return true;
+    }
+
+    public String next() {
+            return null;
+        }
+}
+
